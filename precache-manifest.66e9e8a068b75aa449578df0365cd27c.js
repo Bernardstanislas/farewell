7a63075c6e5ae870c8dbe0f426d0492e4e4d53d6
Stanislas Bernard <bernardstanislas@gmail.com>
2019-01-24 14:43:07 +0100

    Updates

---
+++ precache-manifest.66e9e8a068b75aa449578df0365cd27c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0062422630d01fdebae0",
+    "url": "/farewell/static/js/runtime~main.00624226.js"
+  },
+  {
+    "revision": "cceb511b697b767601a9",
+    "url": "/farewell/static/js/main.cceb511b.chunk.js"
+  },
+  {
+    "revision": "7fe3bef41923022ee146",
+    "url": "/farewell/static/js/1.7fe3bef4.chunk.js"
+  },
+  {
+    "revision": "cceb511b697b767601a9",
+    "url": "/farewell/static/css/main.9db406d6.chunk.css"
+  },
+  {
+    "revision": "f788db625556363ff537b11db5a81810",
+    "url": "/farewell/index.html"
+  }
+];
